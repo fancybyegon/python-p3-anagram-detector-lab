51429de4ec7945a147d9dcfeb70695398a3cb6d8
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-05-23 01:10:04 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word.lower()
+        
+    def match(self,words ):
+         sorted_word =sorted(self.word)
+         matches = [
+             word for word in words
+             if sorted(word.lower())  == sorted_word and word != self.word #using list comprehension instead
+         ]
+         
+         return matches
+    
